1415ca06e8ebeb9ada8f5b610601f5e664ef57aa
Mahima <mahima-1b@hackbal.com>
2020-12-09 13:54:25 +0530

    Updated

---
+++ TeachersCommunity/Controllers/HobbyClassesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeachersCommunity.Controllers
+{
+    public class HobbyClassesController : Controller
+    {
+        // GET: HobbyClasses
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TeachersCommunity/Controllers/TutionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeachersCommunity.Controllers
+{
+    public class TutionController : Controller
+    {
+        // GET: Tution
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
